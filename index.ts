86913ec3803d2c5397fd256b93788f6f046c320b
vipulpathak113 <vipulpathak113@gmail.com>
2023-05-13 19:14:23 +0530

    setting up body parser and corsr

--- index.ts
+++ index.ts
@@ -1,14 +1,37 @@
-import express,{Express,Request,Response} from "express";
+import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
 
-const app:Express = express();
+const app: Express = express();
+import cors from 'cors';
+import bodyParser = require('body-parser');
 
 dotenv.config();
 
-const port = process.env.PORT;
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get("/", (req:Request, res:Response) => {
-  res.send("Hello World3");
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT),
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  synchronize: true,
 });
 
-app.listen(port)
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World3');
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(port);
+    console.log(`⚡ﻋ Data source initalized`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
